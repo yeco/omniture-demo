12253abd97a9729f97276b773a8f412ab46fe54f
Jasson Cascante <jasson.cascante@edify.cr>
2013-05-01 10:51:40 -0400

    deleting buildfiles

--- android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Library/WebServer/Documents/Edify/Clickstream/APUS/omniture_demo/android/bin/AndroidManifest.xml : \
-/Library/WebServer/Documents/Edify/Clickstream/APUS/omniture_demo/android/AndroidManifest.xml \
